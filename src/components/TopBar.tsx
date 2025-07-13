5d1365aa53f309e6a9e6779622874382ccd0d0a7
dj <darrellbrand@gmail.com>
2025-07-13 14:08:17 -0700

    wip

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -11,16 +11,16 @@ export default function TopBar() {
         <h1 className='text-xl text-blue-700 md:text-3xl'>CCTCA</h1></div>
       <div className=" flex flex-row flex-wrap justify-center items-center gap-1.5">
         <a href="#home">
-          <Button className="font-bold md:text-2xl p-1 border-1 border-slate-500 " variant="ghost">Home</Button>
+          <Button className="font-bold md:text-2xl p-1 border-1 border-slate-500 hover:bg-slate-400" variant="ghost">Home</Button>
         </a>
         <a href="#about">
-          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500  " variant="ghost">About</Button>
+          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500  hover:bg-slate-400 " variant="ghost">About</Button>
         </a>
         <a href="#programs">
-          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500" variant="ghost">Programs</Button>
+          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500 hover:bg-slate-400" variant="ghost">Programs</Button>
         </a>
         <a href="#contact">
-          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500" variant="ghost">Contact</Button>
+          <Button className="font-bold p-1 md:text-2xl border-1 border-slate-500 hover:bg-slate-400" variant="ghost">Contact</Button>
         </a>
       </div>
     </div>
