2ec9f99ddb3921f7d34d267f16ffd48306254588
dj <darrellbrand@gmail.com>
2025-07-13 14:09:56 -0700

    fix

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 
 export default function TopBar() {
   return (
-    <div className="w-full px-4 md:px-8 py-4 shadow-sm border-b flex flex-row justify-between items-center  bg-slate-300">
+    <div className="w-full px-4 md:px-8 py-2 shadow-sm border-b flex flex-row justify-between items-center  bg-slate-300">
       <div className=" font-bold flex  items-center ">
         <h1 className='text-xl text-blue-700 md:text-3xl'>CCTCA</h1></div>
       <div className=" flex flex-row flex-wrap justify-center items-center gap-1.5">
