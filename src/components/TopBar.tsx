86f64ca3ffbcce9b79b9709ec816f935cc367142
dj <darrellbrand@gmail.com>
2025-07-16 15:43:52 -0700

    button

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -11,16 +11,16 @@ export default function TopBar() {
         <h1 className='text-xl text-blue-700 md:text-3xl'>CCTCA</h1></div>
       <div className=" flex flex-row flex-wrap justify-center items-center gap-1.5">
         <a href="#home">
-          <Button className=" text-slate-700 font-bold md:text-2xl p-1 border-2 border-slate-400 bg-slate-200 hover:bg-slate-300" variant="ghost">Home</Button>
+          <Button className=" text-slate-700 font-bold md:text-2xl p-1  bg-slate-300 hover:bg-slate-400 " variant="ghost">Home</Button>
         </a>
         <a href="#about">
-          <Button className=" text-slate-700 font-bold p-1 md:text-2xl border-2 border-slate-400 bg-slate-200 hover:bg-slate-300 " variant="ghost">About</Button>
+          <Button className=" text-slate-700 font-bold p-1 md:text-2xl  bg-slate-300 hover:bg-slate-400 " variant="ghost">About</Button>
         </a>
         <a href="#programs">
-          <Button className=" text-slate-700 font-bold p-1 md:text-2xl border-2 border-slate-400 bg-slate-200 hover:bg-slate-300" variant="ghost">Programs</Button>
+          <Button className=" text-slate-700 font-bold p-1 md:text-2xl  bg-slate-300 hover:bg-slate-400" variant="ghost">Programs</Button>
         </a>
         <a href="#contact">
-          <Button className="text-slate-700 font-bold p-1 md:text-2xl border-2 border-slate-400 bg-slate-200 hover:bg-slate-300" variant="ghost">Contact</Button>
+          <Button className="text-slate-700 font-bold p-1 md:text-2xl  bg-slate-300 hover:bg-slate-400" variant="ghost">Contact</Button>
         </a>
       </div>
     </div>
