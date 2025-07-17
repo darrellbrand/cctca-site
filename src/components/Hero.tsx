1bde8289228cdb3f97c82ec33dd889b076f91990
dj <darrellbrand@gmail.com>
2025-07-17 13:36:35 -0700

    update hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,30 @@
 'use client'
 
-import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 
 export default function Hero() {
   return (
-    <section className="w-full h-full flex items-center justify-center  bg-gradient-to-b from-slate-700 to-blue-200  px-4 bg-sl">
-      <div className="w-full text-center space-y-6 ">
-        {/* Logo */}
-        <div className="flex justify-center mt-4">
-          <Image
-            src="/bitmap.svg" // replace with your actual logo path
-            alt="CCTCA Logo"
-            width={500}
-            height={500}
-            priority
-            className=""
-          />
-        </div>
-        <p className="text-lg md:text-2xl text-slate-800 bg-sl mx-auto max-w-7xl">
-          Providing underserved adults with digital literacy, cybersecurity skills, and career training to advance equity and economic opportunity.
-        </p>
-        {/* CTA Button */}
-        <div className="pt-4">
+    <section className="relative w-full bg-black h-[30vh] md:h-[80vh]">
+      {/* Image Wrapper */}
+      <div className="w-full h-full ">
+        <img
+          src="/lab.jpg"
+          alt="CCTCA Lab"
+          className="w-full  object-cover object-center h-full"
+        />
+      </div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-b from-slate-700/80 to-blue-200/20 z-10" />
+
+      {/* Text Overlay */}
+      <div className="absolute  inset-0 z-20 flex flex-col justify-between items-center  py-2 px-2 md:py-16 " >
+        <h1 className="text-3xl md:text-8xl  font-bold text-center drop-shadow-lg text-slate-100  max-w-7xl">
+          Central Coast Tech & Career Academy
+        </h1>
+        <div>
           <a href="#contact">
-            <Button className="bg-slate-200 text-blue-700 text-lg font-semibold hover:bg-slate-300  mb-6  ">
+            <Button className="bg-slate-100 text-blue-700 text-sm md:text-2xl  font-semibold hover:bg-slate-300   md:py-6 md:px-4 ">
               Get Started
             </Button>
           </a>
