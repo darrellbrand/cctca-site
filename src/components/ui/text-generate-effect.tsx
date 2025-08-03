dfa1ef85ed7344ec078a6690e3723fa36c79048d
dj <darrellbrand@gmail.com>
2025-08-03 12:04:28 -0700

    fix warning

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -28,7 +28,7 @@ export const TextGenerateEffect = ({
         delay: stagger(0.2),
       }
     );
-  }, [duration,filter]);
+  }, [duration,filter, animate]);
 
   const renderWords = () => {
     return (
